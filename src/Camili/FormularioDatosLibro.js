be2e72ac767058867c5ea7a6748b0e938b9e0970
Camila <camipereira2111@gmail.com>
2018-10-02 09:25:24 -0300

    avances en guardar data de FormularioDonacion

---
+++ src/Camili/FormularioDatosLibro.js
@@ -0,0 +1,9 @@
+// Get a reference to the database service
+var database = firebase.database();
+
+function saveBookData() {
+    const title = document.getElementById('bookTitleBox').value;
+    const author = document.getElementById('bookAuthorBox').value;
+    const place = document.getElementById('place').value;
+    const review = document.getElementById('reviewBox').value;
+}
