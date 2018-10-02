5f71d58a0d43ad7c1f9a767a10a7aa981602688f
Camila <camipereira2111@gmail.com>
2018-10-02 09:42:05 -0300

    creacion objeto con elementos de libro

--- src/Camili/FormularioDatosLibro.js
+++ src/Camili/FormularioDatosLibro.js
@@ -1,9 +1,19 @@
 // Get a reference to the database service
-var database = firebase.database();
+var db = firebase.firestore();
 
+//aca guardamos los datos ingresador por donante
 function saveBookData() {
     const title = document.getElementById('bookTitleBox').value;
     const author = document.getElementById('bookAuthorBox').value;
     const place = document.getElementById('place').value;
     const review = document.getElementById('reviewBox').value;
+
+    db.collection("Libros").add({
+        title = Title,
+        author = Author,
+        place = Place,
+        review = Review
+    })
+
+
 }
